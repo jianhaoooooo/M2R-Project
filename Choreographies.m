b61c736e76a87e7745b3db86e59876887ed9cee2
Jian Hao <jianhao18@gmail.com>
2019-06-08 23:46:47 +0100

    seperated actiongradeval

--- Choreographies.m
+++ Choreographies.m
@@ -9,55 +9,13 @@ q0 = chebfun(@(t)cos(t)+1i*sin(2*t), [0 2*pi], N,'trig');
 c0 = trigcoeffs(q0);
 
 %%
-% kinetic part of action
-syms(sym('u', [1 N]));
-u = (sym('u',[1 N]));
-syms(sym('v', [1 N]));
-v = (sym('v',[1 N]));
-
-k = -((N-1)/2) : ((N-1)/2);
-Ak_sym = sym('pi')*n * sum(k.*(u.^2 + v.^2));
-
-x=transpose([u,v]);
-
-% verifying that this is indeed agrees with the explicit representation in
-% the book
-gradf = jacobian(Ak_sym,x).';
-
-% potential part of action
-
-Au_sym = 0;
-
-for j= 1:n-1
-    syms t real
-    a_kj = (1-cos(2*sym('pi')*j.*k./n)).*cos(k.*t) + sin(2*sym('pi').*j.*k./n).*sin(k.*t);
-    b_kj = (-1+cos(2*sym('pi')*j.*k./n)).*sin(k.*t) + sin(2*sym('pi').*j.*k./n).*cos(k.*t);
-    f_j = (sum(a_kj .* u + b_kj .* v))^2 + (sum(a_kj .* v - b_kj .* u))^2;
-    tmp = (f_j)^-0.5;
-    
-    
-    % this part won't evaluate properly
-    integral = int(tmp, t, 0, 2*pi);
-    
-    % summation over j
-    Au_sym = Au_sym + n/2 * integral;
-end 
-
-% the parts here won't run properly unless the integral is evaluated
-% in order to have a Au_sym without the variable t
-
-
-% combining kinetic and potential part together 
-A_sym = Ak_sym + Au_sym;
-A = matlabFunction(A_sym, 'vars', {[u v]}); %converting this into an anonymous function to be used for fminunc
-
-
 options = optimoptions('fminunc');
 options.GradObj = 'on';
 options.Algorithm = 'quasi-newton';
 options.HessUpdate = 'bfgs';
-c = fminunc(@(x) actiongradeval(x, n),[real(c0);imag(c0)]',options);
 
+objfunction = actiongradeval(n, N, w)
+c = fminunc(@(x) objfunction(x),[real(c0);imag(c0)]',options);
 
 %%
 % Newton method:
