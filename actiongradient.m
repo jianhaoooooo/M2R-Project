14db2cfa365cb25da152998cee7225a23685073b
Jian Hao <44209304+jianhaoooooo@users.noreply.github.com>
2019-06-13 11:29:00 +0100

    Delete actiongradient.m

--- actiongradient.m
+++
@@ -1,7 +0,0 @@
-function [A, G] = actiongradient(x, A_sym ,G_sym, ck)
-    disp('Optimizing...')
-    A = double(subs(A_sym, ck, x));
-    
-    if nargout >1 
-        G = double(subs(G_sym, ck, x));
-end
